663737f27a3dd011d07153a4f3bf7befbd9edd97
EzekTec <stephen.ezekwem@ezektec.com>
2023-08-16 11:52:33 -0600

    Initial commit of the serverless project on netlify

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use lambda_http::{http::header::CONTENT_TYPE, run, service_fn, Body, Error, Request, Response};
+
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    dbg!(event);
+    let html = "<html><body><h1>Hello from Chinwuba!</h1></body></html>";
+    let resp = Response::builder()
+        .status(200)
+        .header(CONTENT_TYPE, "text/html")
+        .body(Body::Text(html.to_string()))?;
+
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(service_fn(function_handler)).await
+}
